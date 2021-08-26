1c2bcde54324d186c2b0cf8ff01068023badc5d2
ryusaki <alesonofgod@gmail.com>
2021-08-25 20:10:27 -0600

    tema oscuro

---
+++ app.js
@@ -0,0 +1,31 @@
+
+const boton = document.querySelector('#boton');
+
+const configUser = window.matchMedia('(prefers-color-scheme: dark)');
+
+const navegador = localStorage.getItem('tema');
+
+if (navegador === 'dark') {
+    document.body.classList.toggle('dark-theme')
+    }else if(navegador === 'light') {
+    document.body.classList.toggle('light-theme')
+}
+boton.addEventListener ('click', () =>{
+    // console.log(configUser.matches);
+    let colorTema;
+
+    if (configUser.matches) {
+        //Entramos con modo oscuro
+        document.body.classList.toggle('light-theme')
+        
+        colorTema = document.body.classList.contains('light-theme') ? 'light' : 'dark'
+    
+    }else {
+        document.body.classList.toggle('dark-theme')
+        
+        colorTema = document.body.classList.contains('dark-theme') ? 'dark' : 'light'
+
+    }
+    localStorage.setItem('tema', colorTema)
+});
+
